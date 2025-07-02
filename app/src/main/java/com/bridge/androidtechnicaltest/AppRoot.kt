9bd9c00bd1147094d9d2ccba6038e827b54c3c75
chigorgor <ccasadu15@gmail.com>
2025-07-02 12:52:49 +0100

    Implement NavHost with typed destinations for pupil list and signup screens

---
+++ app/src/main/java/com/bridge/androidtechnicaltest/AppRoot.kt
@@ -0,0 +1,75 @@
+package com.bridge.androidtechnicaltest
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.bridge.androidtechnicaltest.ui.login.PupilListScreen
+import com.bridge.androidtechnicaltest.ui.signup.SignUpScreen
+import kotlinx.serialization.Serializable
+
+@Composable
+fun AppRoot() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = HomeDestination
+    ) {
+
+        pupilListScreen(
+            onNavigateToMain = { },
+            onNavigateToSignUp = { id -> navController.navigateToSinglePupil(id) }
+        )
+        pupilScreen(
+            onNavigateToMain = {  },
+            onNavigateUp = { navController.navigateUp() }
+        )
+
+    }
+
+
+}
+
+
+@Serializable
+data object HomeDestination
+
+fun NavGraphBuilder.pupilListScreen(
+    onNavigateToMain: () -> Unit,
+    onNavigateToSignUp: (pupilId: Int) -> Unit,
+) {
+    composable<HomeDestination> {
+        PupilListScreen(
+            onNavigateToMain = onNavigateToMain,
+            onNavigateToPupil = onNavigateToSignUp
+        )
+    }
+}
+
+
+
+@Serializable
+private data class PupilDestination(
+    val pupilId : Int
+)
+
+fun NavGraphBuilder.pupilScreen(
+    onNavigateToMain: () -> Unit,
+    onNavigateUp: () -> Unit
+) {
+    composable<PupilDestination> { backStackEntry ->
+        val emailId = backStackEntry.arguments?.getInt("pupilId")
+        SignUpScreen(
+            pupilId = emailId,
+            onNavigateToMain = onNavigateToMain,
+            onNavigateUp = onNavigateUp
+        )
+    }
+}
+
+fun NavController.navigateToSinglePupil(emailId: Int) {
+    navigate(PupilDestination(emailId))
+}
+
