67382b2fe92ceb9722f7bb384953bde8ec330c1f
chigorgor <ccasadu15@gmail.com>
2025-07-02 16:56:45 +0100

    update viewmodel and UI to collect and display paged pupils

--- app/src/main/java/com/bridge/androidtechnicaltest/ui/login/PupilsScreen.kt
+++ app/src/main/java/com/bridge/androidtechnicaltest/ui/login/PupilsScreen.kt
@@ -1,6 +1,5 @@
 package com.bridge.androidtechnicaltest.ui.login
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
@@ -17,20 +15,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.bridge.androidtechnicaltest.common.BaseResponse
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.bridge.androidtechnicaltest.ui.viewmodel.GetPupilsViewmodel
 import com.chichi.projectsetupapp.ui.theme.AppTheme
 
@@ -41,60 +38,159 @@ fun PupilListScreen(
     onNavigateToPupil: (pupilId: Int?) -> Unit,
     viewModel: GetPupilsViewmodel = hiltViewModel()
 ) {
-    val pupilsState by viewModel.pupilsState.collectAsState()
+    val lazyPagingItems = viewModel.pagedPupils.collectAsLazyPagingItems()
 
-    LaunchedEffect(Unit) {
-        viewModel.loadPupils()
-    }
+    Scaffold(
+        topBar = { PupilSearchBar() },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { onNavigateToPupil(null) },
+                text = { Text("Add new pupil") },
+                icon = { Icon(imageVector = Icons.Default.Add, contentDescription = null) }
+            )
+        }
+    ) { paddingValues ->
 
-    Scaffold(topBar = {
-        PupilSearchBar()
-    }, floatingActionButton = {
-        ExtendedFloatingActionButton(onClick = {onNavigateToPupil(null)},
-            text = { Text("Add new pupil") },
-            icon = { Icon(imageVector = Icons.Default.Add, null) })
-    }) { paddingValues ->
+        Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
 
-        when (val state = pupilsState) {
-            is BaseResponse.Success -> {
-                val pList = state.data
-                Log.d("STUDENTS_TAG", "PupilListScreen: $pList")
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(paddingValues)
-                ) {
-                    items(pList) { pupil ->
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(lazyPagingItems.itemCount) { index ->
+                    val pupil = lazyPagingItems[index]
+                    if (pupil != null) {
+                        ListItem(
+                            modifier = Modifier
+                                .fillMaxWidth().padding(vertical = 20.dp, horizontal = 12.dp)
+                                .clickable { onNavigateToPupil(pupil.pupilId) },
+                            headlineContent = {
+                                Text(pupil.name ?: "Unnamed")
+                            }
+                        )
+                    }
+                }
 
-                        ListItem(modifier = Modifier.clickable {
-                            onNavigateToPupil(
-                                pupil.pupilId
-                            )
-                        },
-                            headlineContent = { Text(pupil.name ?: "Unnamed") })
+                // Append loading indicator
+                lazyPagingItems.apply {
+                    when (loadState.append) {
+                        is LoadState.Loading -> {
+                            item {
+                                Box(
+                                    Modifier
+                                        .fillMaxWidth()
+                                        .padding(16.dp),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    CircularProgressIndicator()
+                                }
+                            }
+                        }
+                        is LoadState.Error -> {
+                            val error = loadState.append as LoadState.Error
+                            item {
+                                Text(
+                                    text = "Error loading more: ${error.error.localizedMessage}",
+                                    color = MaterialTheme.colorScheme.error,
+                                    modifier = Modifier.padding(16.dp)
+                                )
+                            }
+                        }
+                        else -> Unit
                     }
                 }
             }
 
-            is BaseResponse.Loading -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+            // Initial loading
+            when (val state = lazyPagingItems.loadState.refresh) {
+                is LoadState.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.align(Alignment.Center)
+                    )
                 }
 
-            }
+                is LoadState.Error -> {
+                    Text(
+                        text = "Error: ${state.error.localizedMessage}",
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Center).padding(16.dp)
+                    )
+                }
 
-            is BaseResponse.Empty -> {
-                Text("No pupils available", modifier = Modifier.padding(16.dp))
-            }
-
-            is BaseResponse.Error<*> -> {
-                Text("Error loading pupils", modifier = Modifier.padding(16.dp))
+                else -> Unit
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+//
+//@Composable
+//fun PupilListScreen(
+//    onNavigateToMain: () -> Unit,
+//    onNavigateToPupil: (pupilId: Int?) -> Unit,
+//    viewModel: GetPupilsViewmodel = hiltViewModel()
+//) {
+//    val pupilsState by viewModel.pupilsState.collectAsState()
+//
+//    LaunchedEffect(Unit) {
+//        viewModel.loadPupils()
+//    }
+//
+//    Scaffold(topBar = {
+//        PupilSearchBar()
+//    }, floatingActionButton = {
+//        ExtendedFloatingActionButton(onClick = {onNavigateToPupil(null)},
+//            text = { Text("Add new pupil") },
+//            icon = { Icon(imageVector = Icons.Default.Add, null) })
+//    }) { paddingValues ->
+//
+//        when (val state = pupilsState) {
+//            is BaseResponse.Success -> {
+//                val pList = state.data
+//                Log.d("STUDENTS_TAG", "PupilListScreen: $pList")
+//                LazyColumn(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(paddingValues)
+//                ) {
+//                    items(pList) { pupil ->
+//
+//                        ListItem(modifier = Modifier.clickable {
+//                            onNavigateToPupil(
+//                                pupil.pupilId
+//                            )
+//                        },
+//                            headlineContent = { Text(pupil.name ?: "Unnamed") })
+//                    }
+//                }
+//            }
+//
+//            is BaseResponse.Loading -> {
+//                Box(
+//                    modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+//                ) {
+//                    CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+//                }
+//
+//            }
+//
+//            is BaseResponse.Empty -> {
+//                Text("No pupils available", modifier = Modifier.padding(16.dp))
+//            }
+//
+//            is BaseResponse.Error<*> -> {
+//                Text("Error loading pupils", modifier = Modifier.padding(16.dp))
+//            }
+//        }
+//    }
+//}
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
--- app/src/main/java/com/bridge/androidtechnicaltest/ui/viewmodel/GetPupilsViewmodel.kt
+++ app/src/main/java/com/bridge/androidtechnicaltest/ui/viewmodel/GetPupilsViewmodel.kt
@@ -2,11 +2,15 @@ package com.bridge.androidtechnicaltest.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.bridge.androidtechnicaltest.common.BaseResponse
 import com.bridge.androidtechnicaltest.data.model.pupil.local.Pupil
 import com.bridge.androidtechnicaltest.data.model.pupil.local.PupilEntity
 import com.bridge.androidtechnicaltest.domain.GetPupilsUseCase
+import com.bridge.androidtechnicaltest.domain.PupilsPagingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -15,24 +19,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GetPupilsViewmodel @Inject constructor(
-    private val getPupilsUseCase: GetPupilsUseCase
+    private val getPupilsUseCase: GetPupilsUseCase,
+    private val pagingRepository: PupilsPagingRepository
 ) : ViewModel() {
 
-    private val _pupilsState = MutableStateFlow<BaseResponse<List<PupilEntity>>>(BaseResponse.Loading)
-    val pupilsState: StateFlow<BaseResponse<List<PupilEntity>>> = _pupilsState
+
+    val pagedPupils: Flow<PagingData<PupilEntity>> =
+        pagingRepository.getPagedPupils().cachedIn(viewModelScope)
+
 
     private val _pupilByIdState = MutableStateFlow<BaseResponse<Pupil>>(BaseResponse.Loading)
     val pupilByIdState: StateFlow<BaseResponse<Pupil>> = _pupilByIdState
-
-    fun loadPupils(page: Int = 4) {
-
-        viewModelScope.launch {
-            getPupilsUseCase.getPupils(page).collect { response ->
-                _pupilsState.value = response
-            }
-        }
-    }
-
 
     fun loadPupilById(pupilId: Int) {
         viewModelScope.launch {
@@ -41,4 +38,19 @@ class GetPupilsViewmodel @Inject constructor(
             }
         }
     }
+
+    // Manual loading
+
+//    private val _pupilsState = MutableStateFlow<BaseResponse<List<PupilEntity>>>(BaseResponse.Loading)
+//    val pupilsState: StateFlow<BaseResponse<List<PupilEntity>>> = _pupilsState
+//
+//    fun loadPupils(page: Int = 1) {
+//        viewModelScope.launch {
+//            getPupilsUseCase.getPupils(page).collect { response ->
+//                _pupilsState.value = response
+//            }
+//        }
+//    }
+
 }
+
